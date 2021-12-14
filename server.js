fc1bf27658ad25b609784bf625e4eb91f56b0993
Dipti Rani <gdipti642@gmail.com>
2021-12-14 11:57:55 +0530

    done with the project

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer();
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +13,18 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // console.log(req.file);
+  const { originalname, mimetype, size } = req.file;
+  if(!req.file) {
+    res.json({ error: 'Select a file to upload' })
+  }
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
